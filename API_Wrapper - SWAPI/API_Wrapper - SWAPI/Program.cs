e91f5fe438825e24226e5d5fd673fd3297a31ef0
Llayton Titus <92278725+CloaXy@users.noreply.github.com>
2023-11-14 15:53:53 +0200

    Changes made with packages and dependancies

--- API_Wrapper - SWAPI/API_Wrapper - SWAPI/Program.cs
+++ API_Wrapper - SWAPI/API_Wrapper - SWAPI/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.OpenApi.Models;
+using AspNetCoreRateLimit;
 
 var builder = WebApplication.CreateBuilder(args);
 
